2ee82b4f2f3598f1f3bbee7fe150c190d4cae385
Taisei Toya <toyeah0102@gmail.com>
2021-07-25 02:40:33 +0900

    feat: 🎸 philoの食事時間をセットする関数を追加

--- philo_routine.c
+++ philo_routine.c
@@ -1,5 +1,14 @@
 #include "philo.h"
 
+void set_last_meal_time(t_philo *philo)
+{
+	if (gettimeofday(&(philo->last_meal_time), NULL) != 0)
+	{
+		//error
+		exit(1);
+	}
+}
+
 void *philo_routine(void *p)
 {
 	t_philo *philo = (t_philo *)p;
