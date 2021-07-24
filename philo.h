1fd7fbc9c6858c0d925f6bb82659633ed6b97019
Taisei Toya <toyeah0102@gmail.com>
2021-07-25 02:28:02 +0900

    feat: 🎸 philo構造体に最後の食事時間を追加

--- philo.h
+++ philo.h
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <stdlib.h>
+#include <sys/time.h>
 
 typedef struct s_philo t_philo;
 
 typedef struct s_fork t_fork;
+
+typedef struct timeval t_time;
 
 struct s_fork
 {
@@ -17,5 +20,6 @@ struct s_philo
 	t_fork *bigger;
 	t_fork *smaller;
 	pthread_t thread;
+	t_time last_meal_time;
 	int num;
 };
