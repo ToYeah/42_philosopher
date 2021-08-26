bdcfbe1d1d8e0f209a77018c5b23ef7f8b27c5d7
Taisei Toya <tytaisei0102@gmail.com>
2021-08-26 10:33:12 +0200

    refactor: フォークの取り方を修正

--- srcs/philo_routine.c
+++ srcs/philo_routine.c
@@ -1,21 +1,17 @@
 #include "philo.h"
 
-void take_fork(t_philo *philo, t_bool is_bigger)
+void take_fork(t_philo *philo)
 {
-
-	if (is_bigger == TRUE)
-		pthread_mutex_lock(&(philo->bigger->mutex));
-	else
-		pthread_mutex_lock(&(philo->smaller->mutex));
+	pthread_mutex_lock(&(philo->smaller->mutex));
+	record_philo_action(philo, MSG_FORK);
+	pthread_mutex_lock(&(philo->bigger->mutex));
 	record_philo_action(philo, MSG_FORK);
 }
 
-void put_fork(t_philo *philo, t_bool is_bigger)
+void put_fork(t_philo *philo)
 {
-	if (is_bigger == TRUE)
-		pthread_mutex_unlock(&(philo->bigger->mutex));
-	else
-		pthread_mutex_unlock(&(philo->smaller->mutex));
+	pthread_mutex_unlock(&(philo->bigger->mutex));
+	pthread_mutex_unlock(&(philo->smaller->mutex));
 }
 
 void eat_meal(t_philo *philo)
@@ -39,14 +35,16 @@ void *philo_routine(void *p)
 {
 	t_philo *philo = (t_philo *)p;
 
+	if (philo->num % 2 == 0)
+	{
+		philo_usleep(1);
+	}
 	philo->last_meal_time = get_time_in_ms();
 	while (philo->rule->dead_exists == FALSE)
 	{
-		take_fork(philo, FALSE);
-		take_fork(philo, TRUE);
+		take_fork(philo);
 		eat_meal(philo);
-		put_fork(philo, TRUE);
-		put_fork(philo, FALSE);
+		put_fork(philo);
 		sleep_philo(philo);
 		think_philo(philo);
 	}
