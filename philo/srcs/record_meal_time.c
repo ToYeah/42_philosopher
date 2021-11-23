0b722fbbc2d5cf161c6be04b325faa4affefd4d2
Taisei Toya <tytaisei0102@gmail.com>
2021-11-23 21:38:39 +0100

    norm対応

--- philo/srcs/record_meal_time.c
+++ philo/srcs/record_meal_time.c
@@ -1,18 +1,18 @@
 #include "philo.h"
 
-void write_meal_time(t_philo *philo, long time)
+void	write_meal_time(t_philo *philo, long time)
 {
 	pthread_mutex_lock(&(philo->right_to_meal_time));
 	philo->last_meal_time = time;
 	pthread_mutex_unlock(&(philo->right_to_meal_time));
 }
 
-long read_meal_time(t_philo *philo)
+long	read_meal_time(t_philo *philo)
 {
-	long res;
+	long	res;
 
 	pthread_mutex_lock(&(philo->right_to_meal_time));
 	res = philo->last_meal_time;
 	pthread_mutex_unlock(&(philo->right_to_meal_time));
-	return res;
+	return (res);
 }
