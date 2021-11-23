dde84f850f4b054d4eab7c527280615a681b28b4
Taisei Toya <tytaisei0102@gmail.com>
2021-11-23 21:49:08 +0100

    norm対応

--- philo/srcs/thread_utils.c
+++ philo/srcs/thread_utils.c
@@ -1,41 +1,42 @@
 #include "philo.h"
 
-long get_time_in_us()
+long	get_time_in_us(void)
 {
-	struct timeval now;
+	struct timeval	now;
 
 	gettimeofday(&now, NULL);
 	return ((long)(now.tv_sec * 1000000 + now.tv_usec));
 }
 
-long get_time_in_ms()
+long	get_time_in_ms(void)
 {
-	return(get_time_in_us() / 1000);
+	return (get_time_in_us() / 1000);
 }
 
-void philo_usleep(long time)
+void	philo_usleep(long time)
 {
-	long start;
-	long now;
+	long	start;
+	long	now;
 
 	start = get_time_in_us();
-	while(1)
+	while (1)
 	{
 		now = get_time_in_us();
 		if (now - start >= time * 1000)
-			break;
+			break ;
 		usleep(1000);
 	}
 }
 
-long record_philo_action(t_philo *philo, const char *str)
+long	record_philo_action(t_philo *philo, const char *str)
 {
-	long time;
+	long	time;
 
 	pthread_mutex_lock(&(philo->rule->right_to_output));
 	time = get_time_in_ms();
-	if (philo->rule->dead_exists == FALSE && philo->rule->full_philo_count != philo->rule->num)
+	if (philo->rule->dead_exists == FALSE
+		&& philo->rule->full_philo_count != philo->rule->num)
 		printf("%ld %ld %s\n", time, philo->num, str);
 	pthread_mutex_unlock(&(philo->rule->right_to_output));
-	return time;
+	return (time);
 }
