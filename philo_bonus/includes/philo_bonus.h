71da5ad278bb7de0c957997d8436bafa1a3cc4be
Taisei Toya <tytaisei0102@gmail.com>
2021-11-24 18:45:21 +0100

    norm対応

--- philo_bonus/includes/philo_bonus.h
+++ philo_bonus/includes/philo_bonus.h
@@ -78,4 +78,10 @@ void	philo_routine(t_philo *philo);
 void	*nurse_routine(void *p);
 void	*doctor_routine(void *p);
 
+void	take_fork(t_philo *philo);
+void	put_fork(t_philo *philo);
+void	eat_meal(t_philo *philo);
+void	sleep_philo(t_philo *philo);
+void	think_philo(t_philo *philo);
+
 #endif
